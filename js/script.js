3f4dd5ac090de8131a3cf0a78d425d34f31c069f
Crista-Desuraune <cdesuraunecocu@normandiewebschool.fr>
2025-02-27 01:51:27 +0100

    modif header menu burger > à poursuivre

--- js/script.js
+++ js/script.js
@@ -127,12 +127,11 @@ var closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
 function openNav() {
     sidenav.classList.add("active");
 }
 
-/* Set the width of the side navigation to 0 */
+
 function closeNav() {
     sidenav.classList.remove("active");
 }

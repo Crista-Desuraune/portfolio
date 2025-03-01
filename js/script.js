1f03d0ce3be62ae4c1584cc60ba4c77846cf91d5
Crista-Desuraune <cdesuraunecocu@normandiewebschool.fr>
2025-03-02 00:12:00 +0100

    reglages problemes carrousel+header(ban)

--- js/script.js
+++ js/script.js
@@ -1,137 +1,120 @@
-// const buttons = document.querySelectorAll(".btn");
-// const slides = document.querySelectorAll(".slide");
+function carrousel() {
+    let nbr = 8;
+    let p = 0;
+    let container = document.getElementById("container");
+    let g = document.getElementById("g");
+    let d = document.getElementById("d");
 
-// // Tableau d'image : [0, 1, 2]
+    container.style.width = (600 * nbr) + "px";
+    for (let i = 1; i <= nbr; i++) {
+        let div = document.createElement("div");
+        div.className = "photo";
+        div.style.backgroundImage = "url('../images/rachid/rachid" + i + ".jpg')";
+        container.appendChild(div);
+    }
 
-// buttons.forEach((button) => {
-//     button.addEventListener("click", (e) => {
-//         const calcNextSlide = e.target.id === "next" ? 1 : -1;
-//         const slideActive = document.querySelector(".active");
+    function afficherMasquer() {
+        d.style.visibility = (p == -nbr + 1) ? "hidden" : "visible";
+        g.style.visibility = (p == 0) ? "hidden" : "visible";
+    }
 
-//         console.log(e.target.id);
+    d.onclick = function () {
+        if (p > -nbr + 1) p--;
+        container.style.transform = "translate(" + p * 600 + "px)";
+        container.style.transition = "all 0.5s ease";
+        afficherMasquer();
+    }
 
-//         newIndex = calcNextSlide + [...slides].indexOf(slideActive);
+    g.onclick = function () {
+        if (p < 0) p++;
+        container.style.transform = "translate(" + p * 600 + "px)";
+        container.style.transition = "all 0.5s ease";
+        afficherMasquer();
+    }
 
-//         if (newIndex < 0) newIndex = [...slides].length - 1;
-//         if (newIndex >= [...slides].length) newIndex = 0;
-//         slides[newIndex].classList.add("active");
+    afficherMasquer();
+}
 
-//         slideActive.classList.remove("active");
-//     });
+function carrousseltel() {
+    let nbr = 8;
+    let p = 0;
+    let container = document.getElementById("containertel");
+    let gtel = document.getElementById("gtel");
+    let dtel = document.getElementById("dtel");
+
+    container.style.width = (300 * nbr) + "px";
+    for (let i = 1; i <= nbr; i++) {
+        let div = document.createElement("div");
+        div.className = "photo";
+        div.style.backgroundImage = "url('../images/rachid/rachid" + i + ".jpg')";
+        container.appendChild(div);
+    }
+
+    function afficherMasquerTel() {
+        dtel.style.visibility = (p == -nbr + 1) ? "hidden" : "visible";
+        gtel.style.visibility = (p == 0) ? "hidden" : "visible";
+    }
+
+    dtel.onclick = function () {
+        if (p > -nbr + 1) p--;
+        container.style.transform = "translate(" + p * 300 + "px)";
+        container.style.transition = "all 0.5s ease";
+        afficherMasquerTel();
+    }
+
+    gtel.onclick = function () {
+        if (p < 0) p++;
+        container.style.transform = "translate(" + p * 300 + "px)";
+        container.style.transition = "all 0.5s ease";
+        afficherMasquerTel();
+    }
+
+    afficherMasquerTel();
+}
+
+function carrouselces() {
+    let nbr = 12;
+    let p = 0;
+    let container = document.getElementById("containerces");
+    let gces = document.getElementById("gces");
+    let dces = document.getElementById("dces");
+
+    container.style.width = (570 * nbr) + "px";
+    for (let i = 1; i <= nbr; i++) {
+        let div = document.createElement("div");
+        div.className = "photoces";
+        div.style.backgroundImage = "url('../images/ces/ces" + i + ".png')";
+        container.appendChild(div);
+    }
+
+    function afficherMasquerCes() {
+        dces.style.visibility = (p == -nbr + 1) ? "hidden" : "visible";
+        gces.style.visibility = (p == 0) ? "hidden" : "visible";
+    }
+
+    dces.onclick = function () {
+        if (p > -nbr + 1) p--;
+        container.style.transform = "translate(" + p * 570 + "px)";
+        container.style.transition = "all 0.5s ease";
+        afficherMasquerCes();
+    }
+
+    gces.onclick = function () {
+        if (p < 0) p++;
+        container.style.transform = "translate(" + p * 570 + "px)";
+        container.style.transition = "all 0.5s ease";
+        afficherMasquerCes();
+    }
+
+    afficherMasquerCes();
+}
+
+// window.addEventListener("DOMContentLoaded", function () {
+//     carrouselces();
+//     carrousel();
+//     carrousseltel();
 // });
 
-
-// document.body.onload=function(){
-//     nbr=8;
-//     p=0;
-//     container=document.getElementById("container");
-//     g=document.getElementById("g");
-//     d=document.getElementById("d");
-//     container.style.width=(800*nbr)+"px";
-//     for(i=1;i<=nbr;i++){
-//         div=document.createElement("div");
-//         div.className="photo";
-//         div.style.backgroundImage="url('/images/rachid"+i+".jpg')";
-//         container.appendChild(div);
-//     }
-//     afficherMasquer();
-// }
-// g.onclick=function(){
-//     if(p>-nbr+1)
-//         p--;
-//     container.style.transform="translate("+p*600+"px)";
-//     container.style.transition="all 0.5s ease";
-//     afficherMasquer();
-// }
-// d.onclick=function(){
-//     if(p<0)
-//         p++;
-//     container.style.transform="translate("+p*600+"px)";
-//     container.style.transition="all 0.5s ease";
-//     afficherMasquer();
-// }
-// function afficherMasquer(){
-//     if(p==-nbr+1)
-//         g.style.visibility="hidden";
-//     else
-//         g.style.visibility="visible";
-//     if(p==0)
-//         d.style.visibility="hidden";
-//     else
-//         d.style.visibility="visible";
-// }
-function carrousselces(){
-    nbr=12;
-    p=0;
-    container=document.getElementById("containerces");
-    g=document.getElementById("g");
-    d=document.getElementById("d");
-    container.style.width=(570*nbr)+"px";
-    for(i=1;i<=nbr;i++){
-        div=document.createElement("div");
-        div.className="photoces";
-        div.style.backgroundImage="url('../images/ces/ces"+i+".png')";
-        container.appendChild(div);
-    }
-    afficherMasquer();
-}
-function carroussel(){
-    nbr=8;
-    p=0;
-    container=document.getElementById("container");
-    g=document.getElementById("g");
-    d=document.getElementById("d");
-    container.style.width=(600*nbr)+"px";
-    for(i=1;i<=nbr;i++){
-        div=document.createElement("div");
-        div.className="photo";
-        div.style.backgroundImage="url('../images/rachid/rachid"+i+".jpg')";
-        container.appendChild(div);
-    }
-    afficherMasquer();
-}
-d.onclick=function(){
-    if(p>-nbr+1)
-        p--;
-    container.style.transform="translate("+p*570+"px)";
-    container.style.transition="all 0.5s ease";
-    afficherMasquer();
-}
-g.onclick=function(){
-    if(p<0)
-        p++;
-    container.style.transform="translate("+p*570+"px)";
-    container.style.transition="all 0.5s ease";
-    afficherMasquer();
-}
-function afficherMasquer(){
-    if(p==-nbr+1)
-        d.style.visibility="hidden";
-    else
-        d.style.visibility="visible";
-    if(p==0)
-        g.style.visibility="hidden";
-    else
-        g.style.visibility="visible";
-}
-
-window.addEventListener("DOMContentLoaded", function (){
-    carrousselces()
-    carroussel()
-});
-
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
-
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
-
-function openNav() {
-    sidenav.classList.add("active");
-}
-
-
-function closeNav() {
-    sidenav.classList.remove("active");
-}
+window.addEventListener("DOMContentLoaded", carrouselces);
+window.addEventListener("DOMContentLoaded", carrousel);
+window.addEventListener("DOMContentLoaded", carrousseltel);

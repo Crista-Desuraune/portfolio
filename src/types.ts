bbb27dd66499cb250f76626b5717ece3d0ef8ce2
Crista-Desuraune <cdesuraunecocu@normandiewebschool.fr>
2025-05-13 21:34:12 +0200

    Fix: -recupération du commit perdu

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export type CarouselType = {
+    imageSrc: string,
+    imageAlt: string
+}
+
+export type SocialItemType = {
+    label: string,
+    url: string,
+    icon: string
+}
+
+export type LinkPrimaryType = {
+    url: string;
+    label: string;
+}
+
+export type CardsType = {
+    label: string;
+    description: string;
+    url: string;
+    image: string;
+}
+
+export type DessinsType = {
+    img: string;
+    alt: string;
+}
+
+export type SkillsType = {
+    label: string;
+    img: string;
+}
